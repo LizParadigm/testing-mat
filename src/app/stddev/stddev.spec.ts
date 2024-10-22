677c5ced917301fb514fb4f2595a60897e6fde95
LizParadigm <jesss635000@gmail.com>
2024-10-21 19:51:49 -0600

    nose

--- src/app/stddev/stddev.spec.ts
+++ src/app/stddev/stddev.spec.ts
@@ -1,13 +1,13 @@
 import { calculateStdDev } from './stddev';
 
 describe('calculateStdDev', () => {
-    it('should return the correct standard deviation for column 1', () => {
+    it('should return the correct standard deviation = 572.03 for column = [160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503]', () => {
         const col1 = [160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503];
         const stdDevCol1 = calculateStdDev(col1);
         expect(stdDevCol1).toBeCloseTo(572.03, 2);
     });
 
-    it('should return the correct standard deviation for column 2', () => {
+    it('should return the correct standard deviation = 62.26 for column = [15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2]', () => {
         const col2 = [15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2];
         const stdDevCol2 = calculateStdDev(col2);
         expect(stdDevCol2).toBeCloseTo(62.26, 2);
--- src/app/stddev/stddev.ts
+++ src/app/stddev/stddev.ts
@@ -1,5 +1,8 @@
+import { calculateMean } from "../media/media";
+
 export function calculateStdDev(numbers: number[]): number {
-    const mean = numbers.reduce((sum, value) => sum + value, 0) / numbers.length;
+    const mean = calculateMean(numbers);
+
     const squaredDiffs = numbers.map(value => Math.pow(value - mean, 2));
     const avgSquaredDiff = squaredDiffs.reduce((sum, value) => sum + value, 0) / (numbers.length - 1);
     return Math.sqrt(avgSquaredDiff);
