c4c18e4f24227be559d21fade14f82dec5d94235
LizParadigm <jesss635000@gmail.com>
2024-10-22 10:38:04 -0600

    add common direct to linear_regression branch #14

---
+++ src/app/common/calculate.ts
@@ -0,0 +1,15 @@
+
+export function sumX(xData: number[]): number {
+  return xData.reduce((acc, curr) => acc + curr, 0);
+}
+export function sumY(y: number[]): number {
+  return y.reduce((acc, val) => acc + val, 0);
+}
+
+export function sumXX(x: number[]) {
+  return x.reduce((acc, val) => acc + val ** 2, 0);
+}
+
+export function sumXY(x: number[], y: number[]): number {
+  return x.reduce((acc, val, index) => acc + val * y[index], 0);
+}
