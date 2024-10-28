b8fd43bfccd5baa0504980a5c9e03702a0359174
LizParadigm <jesss635000@gmail.com>
2024-10-28 16:44:32 -0600

    update branch

---
+++ src/app/common/simpson_rule.ts
@@ -0,0 +1,59 @@
+export function simpson(x0: number, x1: number, n: number, func: (x: number) => number): number {
+
+    const h = (x1 - x0) / n;
+    let sum = func(x0) + func(x1);
+
+    for (let i = 1; i < n; i++) {
+        const x = x0 + i * h;
+        sum += (i % 2 === 0 ? 2 : 4) * func(x);
+    }
+
+    return (h / 3) * sum;
+}
+
+export function errorSimpson(x0: number, x1: number, n: number, func: (x: number) => number, error: number): number {
+    let previousResult = 0;
+    let currentResult = 0;
+
+    while (currentResult === 0 || Math.abs(currentResult - previousResult) > error) {
+        previousResult = currentResult || simpson(x0, x1, n, func);
+        n *= 2;
+        currentResult = simpson(x0, x1, n, func);
+    }
+
+    return parseFloat(currentResult.toFixed(3));
+}
+
+
+
+export function twoX(x: number): number {
+    return 2 * x;
+}
+
+export function x2(x: number): number {
+    return x * x;
+}
+
+export function oneOverX(x: number): number {
+    return 1 / x;
+}
+
+
+
+function gamma(x: number): number {
+    // Γ(x) = (x - 1)!
+    if (Number.isInteger(x)) {
+        let result = 1;
+        for (let i = 1; i < x; i++) {
+            result *= i;
+        }
+        return result;
+    }
+    // aproximación de Stirling
+    else {
+        const pi = Math.PI;
+        return Math.sqrt(2 * pi / x) * Math.pow(x / Math.E, x);
+    }
+}
+
+
---
+++ src/app/simpson/simpson.component.spec.ts
@@ -0,0 +1,29 @@
+import { TestBed } from '@angular/core/testing';
+import { SimpsonComponent } from './simpson.component';
+import { twoX, x2, oneOverX } from "../common/simpson_rule";
+
+describe("SimpsonComponent Tests", () => {
+  let component: SimpsonComponent;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [SimpsonComponent] // Cambiado de 'declarations' a 'imports'
+    });
+    component = TestBed.createComponent(SimpsonComponent).componentInstance;
+  });
+
+  it("Should return p=16.0 when f(x)=2x, x0=0, x1=4, num_seg=4, ERROR=0.0001", () => {
+    const result = component.calculateSimpsonRule(twoX, 0, 4, 4, 0.0001);
+    expect(parseFloat(result.toFixed(1))).toBe(16.0);
+  });
+
+  it("Should return p=0.3333 when f(x)=x^2, x0=0, x1=1, num_seg=4, ERROR=0.0001", () => {
+    const result = component.calculateSimpsonRule(x2, 0, 1, 4, 0.0001);
+    expect(parseFloat(result.toFixed(4))).toBe(0.3333);
+  });
+
+  it("Should return p=1.38 when f(x)=1/x, x0=1, x1=4, num_seg=6, ERROR=0.001", () => {
+    const result = component.calculateSimpsonRule(oneOverX, 1, 4, 6, 0.001);
+    expect(parseFloat(result.toFixed(2))).toBe(1.38);
+  });
+});
